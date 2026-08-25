2174d8e6f7937c3e4707fcb521fcb49f4f05d7b3
chivo <purpcaps@gmail.com>
2026-08-25 16:56:43 -0600

    feat(security): install API response hardening

--- cloud-server/server.js
+++ cloud-server/server.js
@@ -3,7 +3,9 @@
 const express = require("express");
 const { installHttpContainment } = require("./http-containment");
 const { installProductiveTempAuthBoundary } = require("./productive-temp-auth-boundary");
+const { installSecurityHeaders } = require("./security-headers");
 
+installSecurityHeaders(express);
 installHttpContainment(express, { dataDir: __dirname });
 installProductiveTempAuthBoundary(express);
 require("./server-core");
