1acb251ef1c70fc5e67ac10d10bae2ad48fa1044
chivo <purpcaps@gmail.com>
2026-08-26 01:26:12 -0600

    feat(data): gate cloud startup on postgres migrations

--- cloud-server/server.js
+++ cloud-server/server.js
@@ -4,8 +4,24 @@ const express = require("express");
 const { installHttpContainment } = require("./http-containment");
 const { installProductiveTempAuthBoundary } = require("./productive-temp-auth-boundary");
 const { installSecurityHeaders } = require("./security-headers");
+const { postgresConfig } = require("./postgres-runtime-config");
+const { startPostgresControlPlane, installPostgresShutdown } = require("./postgres-bootstrap");
 
 installSecurityHeaders(express);
 installHttpContainment(express, { dataDir: __dirname });
 installProductiveTempAuthBoundary(express);
-require("./server-core");
+
+const pgConfig = postgresConfig(process.env);
+if (!pgConfig.enabled) {
+  require("./server-core");
+} else {
+  startPostgresControlPlane()
+    .then(({ pool }) => {
+      installPostgresShutdown(pool);
+      require("./server-core");
+    })
+    .catch((error) => {
+      console.error("[postgres] startup failed; cloud-server will not start:", error?.message || String(error));
+      process.exitCode = 1;
+    });
+}
